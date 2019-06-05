f2c9c352da8c420824286478ec429b8dc49cac4c
Lander24 <Lander24@users.noreply.github.com>
2019-06-05 11:01:16 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,3 +6,15 @@ Datediff(Curdate(), rv.Checkin) - 180 ELSE 0 END) ) / 180, 2) Popularity
 FROM   lab7_rooms rm 
        JOIN lab7_reservations rv 
          ON rm.RoomCode = rv.Room GROUP BY rm.RoomCode;
+         
+         
+         --R1 (Not sorted yet)
+         
+         select room,
+round(sum(datediff(least(checkout, curdate()), greatest(checkin,DATE_SUB(curdate(), Interval 180 Day)))/180),2) as pop
+
+from lab7_reservations
+where checkout > DATE_SUB(curdate(), Interval 180 Day) and checkin < curdate()
+group by room
+order by pop desc;
+                         
