3b0e885c4a57035bd51efcbd6c745b95e2a3e3da
gwayne9955 <gwayne9955@yahoo.com>
2019-06-04 17:13:10 -0700

    first commit

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export CLASSPATH=:mysql-connector-java-8.0.16.jar:.
+export L7_JDBC_URL=jdbc:mysql://db.labthreesixfive.com/gwayne?autoReconnect=true\&useSSL=false
+export L7_JDBC_USER=gwayne
+export L7_JDBC_PW=S19_CSC-365-012591536
+javac Lab7.java && java Lab7
