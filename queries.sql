dc697dcad62543b9fb578e39881676fe578378e0
Garrett Wayne <gwayne9955@yahoo.com>
2019-06-05 10:22:32 -0700

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,7 @@
+select rm.RoomCode, 
+round(sum(case when datediff(CURDATE(), rv.Checkout) <= 180 and datediff(CURDATE(), rv.Checkout) >= 0
+                then datediff(rv.Checkout, rv.Checkin) 
+                else 0 
+                end) / 180, 2) Popularity 
+from lab7_rooms rm join lab7_reservations rv on rm.RoomCode = rv.Room
+group by rm.RoomCode;
